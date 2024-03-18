a25d5f129c1ee4df8d46b2fafcbc6368762964ee
andro-jr <prabin20panta@gmail.com>
2024-03-18 20:25:38 +0545

    nav and footer template

--- router/router.js
+++ router/router.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+// Define custom middleware to add current year to response locals
+router.use((req, res, next) => {
+  res.locals.currentYear = new Date().getFullYear();
+  next();
+});
+
 router.get("/", (req, res) => {
   res.render("index", {
-    pagename: "Home",
+    pagename: "Home"
+  });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", {
+    pagename: "About"
   });
 });
 
