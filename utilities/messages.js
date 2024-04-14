39ecbb49f89589ab3398db5adc6d2c3c48c6632e
andro-jr <prabin20panta@gmail.com>
2024-04-14 19:35:50 +0545

    additional login validations

--- utilities/messages.js
+++ utilities/messages.js
@@ -1,7 +1,7 @@
 module.exports = {
     successful_register: 'Registration Successful',
     failed_register: 'Registration Failed',
-    successful_login: 'Login Failed',
+    successful_login: 'Login Successful',
     failed_login: 'Login Failed',
 }
 
